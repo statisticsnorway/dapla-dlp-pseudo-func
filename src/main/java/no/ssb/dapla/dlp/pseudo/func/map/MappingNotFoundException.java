f372aa402a272a8e114caf0d03e84cb6aaf64cf8
Bjørn-Andre Skaar <31540110+bjornandre@users.noreply.github.com>
2024-04-09 08:17:58 +0200

    Add MapFailureStrategy option (#21)
    * Add MapFailureStrategy option that will control the behaviour of MapFunc and MapAndEncryptFunc. The change will also influence classes that implements the Mapper interface. Remove MappingNotFoundException from the Mapper interface since it should no longer be used for mapping failures.

--- src/main/java/no/ssb/dapla/dlp/pseudo/func/map/MappingNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package no.ssb.dapla.dlp.pseudo.func.map;
-
-public class MappingNotFoundException extends RuntimeException {
-    public MappingNotFoundException(String message) {
-        super(message);
-    }
-}
--- src/test/java/no/ssb/dapla/dlp/pseudo/func/composite/MapAndEncryptFuncTest.java
+++ src/test/java/no/ssb/dapla/dlp/pseudo/func/composite/MapAndEncryptFuncTest.java
@@ -8,8 +8,9 @@ import no.ssb.dapla.dlp.pseudo.func.PseudoFuncInput;
 import no.ssb.dapla.dlp.pseudo.func.PseudoFuncOutput;
 import no.ssb.dapla.dlp.pseudo.func.fpe.FpeFunc;
 import no.ssb.dapla.dlp.pseudo.func.fpe.FpeFuncConfig;
+import no.ssb.dapla.dlp.pseudo.func.map.MapFailureStrategy;
 import no.ssb.dapla.dlp.pseudo.func.map.MapFunc;
-import no.ssb.dapla.dlp.pseudo.func.map.MappingNotFoundException;
+import no.ssb.dapla.dlp.pseudo.func.map.MapFuncConfig;
 import no.ssb.dapla.dlp.pseudo.func.map.TestMapper;
 import org.junit.jupiter.api.Test;
 
@@ -19,16 +20,17 @@ import static org.junit.jupiter.api.Assertions.*;
 public class MapAndEncryptFuncTest {
     private static final String BASE64_ENCODED_KEY = "w0/G6A5e/KHtTo31FD6mhhS1Tkga43l79IBK24gM4F8=";
 
-    final PseudoFuncConfig config = new PseudoFuncConfig(ImmutableMap.of(
-            PseudoFuncConfig.Param.FUNC_DECL, "map-fpe-test",
-            PseudoFuncConfig.Param.FUNC_IMPL, MapAndEncryptFunc.class.getName(),
-            MapAndEncryptFuncConfig.Param.MAP_FUNC_IMPL, MapFunc.class.getName(),
-            MapAndEncryptFuncConfig.Param.ENCRYPTION_FUNC_IMPL, FpeFunc.class.getName(),
-            FpeFuncConfig.Param.ALPHABET, "alphanumeric+whitespace",
-            FpeFuncConfig.Param.KEY_ID, "keyId1",
-            FpeFuncConfig.Param.KEY_DATA, BASE64_ENCODED_KEY
-
-    ));
+    private PseudoFuncConfig getConfig() {
+        return new PseudoFuncConfig(ImmutableMap.of(
+                PseudoFuncConfig.Param.FUNC_DECL, "map-fpe-test",
+                PseudoFuncConfig.Param.FUNC_IMPL, MapAndEncryptFunc.class.getName(),
+                MapAndEncryptFuncConfig.Param.MAP_FUNC_IMPL, MapFunc.class.getName(),
+                MapAndEncryptFuncConfig.Param.ENCRYPTION_FUNC_IMPL, FpeFunc.class.getName(),
+                FpeFuncConfig.Param.ALPHABET, "alphanumeric+whitespace",
+                FpeFuncConfig.Param.KEY_ID, "keyId1",
+                FpeFuncConfig.Param.KEY_DATA, BASE64_ENCODED_KEY
+        ));
+    }
     /*
      * This test should transform OriginalValue -> MappedValue -> FPE encrypted MappedValue
      * (and back)
@@ -36,7 +38,7 @@ public class MapAndEncryptFuncTest {
     @Test
     public void transformAndRestore() {
         String expectedVal = "ygd1M9at1nK"; // FPE encrypted MappedValue
-        PseudoFunc func = PseudoFuncFactory.create(config);
+        PseudoFunc func = PseudoFuncFactory.create(getConfig());
 
         PseudoFuncOutput pseudonymized = func.apply(PseudoFuncInput.of(TestMapper.ORIGINAL));
         assertThat(pseudonymized.getValue()).isEqualTo(expectedVal);
@@ -46,21 +48,40 @@ public class MapAndEncryptFuncTest {
     }
 
     @Test
-    public void call_apply_with_mapping_failure() {
+    public void transformAndRestoreWithMissingStrategyIgnore() {
+        // Using MapFailureStrategy.IGNORE one can successfully map and encrypt "gibberish"
+        // and get the same decrypted result back - although the Mapper function didn't find
+        // a mapping for "gibberish"
+        String originalVal = "gibberish";
+        String expectedVal = "J0LjYPO9f"; // FPE encrypted originalVal
+        final PseudoFuncConfig config = getConfig();
+        config.add(MapFuncConfig.Param.MAP_FAILURE_STRATEGY, MapFailureStrategy.RETURN_ORIGINAL);
         PseudoFunc func = PseudoFuncFactory.create(config);
 
-        assertThrows(MappingNotFoundException.class,() ->
-            func.apply(PseudoFuncInput.of("unknown"))
-        );
+        PseudoFuncOutput pseudonymized = func.apply(PseudoFuncInput.of(originalVal));
+        assertThat(pseudonymized.getValue()).isEqualTo(expectedVal);
+
+        PseudoFuncOutput depseudonymized = func.restore(PseudoFuncInput.of(pseudonymized.getValue()));
+        assertThat(depseudonymized.getValue()).isEqualTo(originalVal);
+    }
+
+
+    @Test
+    public void call_apply_with_mapping_failure() {
+        final PseudoFuncConfig config = getConfig();
+        config.add(MapFuncConfig.Param.MAP_FAILURE_STRATEGY, MapFailureStrategy.RETURN_NULL);
+        PseudoFunc func = PseudoFuncFactory.create(config);
+
+        assertThat(func.apply(PseudoFuncInput.of("unknown")).getValue()).isNull();
     }
 
     @Test
     public void call_restore_with_mapping_failure() {
+        final PseudoFuncConfig config = getConfig();
+        config.add(MapFuncConfig.Param.MAP_FAILURE_STRATEGY, MapFailureStrategy.RETURN_NULL);
         PseudoFunc func = PseudoFuncFactory.create(config);
 
-        assertThrows(MappingNotFoundException.class,() ->
-                func.restore(PseudoFuncInput.of("unknown"))
-        );
+        assertThat(func.restore(PseudoFuncInput.of("unknown")).getValue()).isNull();
     }
 
 }
